70cdafe0fde4ebfb4a2f758f4de6f76af297211d
Binh <ttbinh@hueic.edu.vn>
2018-04-17 23:17:54 +0700

    Finish Index page

---
+++ templates/share.php
@@ -0,0 +1,35 @@
+<div class="pls-share">
+  <div class="row">
+    <div class="col-md-12">
+      <h2>If you liked us, others may also like</h2>
+    </div>
+    <h2>PLEASE SHARE</h2>
+  </div>
+  <div class="row post-share">
+      <div class="col-md-3">
+        <a href="" class="facebook">
+          <i class="fa fa-facebook"></i>
+          Facebook
+        </a>
+      </div>
+      <div class="col-md-3">
+        <a href="" class="google">
+          <i class="fa fa-google"></i>
+          Google+
+        </a>
+      </div>
+      <div class="col-md-3">
+        <a href="" class="twitter">
+          <i class="fa fa-twitter"></i>
+          Twitter
+        </a>
+      </div>
+      <div class="col-md-3">
+        <a href="" class="linkedin">
+          <i class="fa fa-linkedin"></i>
+          LinkedIn
+        </a>
+      </div>
+  </div>
+</div>
+<!-- End pls-share -->
